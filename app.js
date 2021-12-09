2ae93cfe0a53e839382c0b8964c33fb62f0a1242
Alice Hsing <alicehsing@gmail.com>
2021-12-08 18:13:54 -0800

    add pure functions renderPoll() and renderOption()

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 // import functions and grab DOM elements
-import { renderPastPoll } from './render-utils.js';
+import { renderPoll } from './render-utils.js';
 
 const form = document.querySelector('form');
-
 const optionAAddButton = document.querySelector('#option-a-add');
 const optionBAddButton = document.querySelector('#option-b-add');
 const optionAUndoButton = document.querySelector('#option-a-undo');
 const optionBUndoButton = document.querySelector('#option-b-undo');
-
 const questionEl = document.querySelector('#poll-question');
 const optionATitleEl = document.querySelector('#option-a-title');
 const optionBTitleEl = document.querySelector('#option-b-title');
 const optionAVotesEl = document.querySelector('#option-a-votes');
 const optionBVotesEl = document.querySelector('#option-b-votes');
-
 const closePollButton = document.querySelector('#close-poll');
-
 const pastPollsEl = document.querySelector('#past-polls');
 
 // let state
@@ -24,9 +20,9 @@ let optionBVotes = 0;
 let optionATitle = '';
 let optionBTitle = '';
 let question = '';
+let pastPollsArray = [];
 
 // set event listeners 
-
 // increment or decrement the vote count for option A & option B in the current poll
 optionAAddButton.addEventListener('click', () => {
     optionAVotes++;
@@ -53,35 +49,66 @@ optionBUndoButton.addEventListener('click', () => {
 form.addEventListener('submit', (e) => {
     //prevent weird behavior from happening  
     e.preventDefault();
-    
     const data = new FormData(form);
     //update state
     question = data.get('poll-question');
     optionATitle = data.get('poll-option-a');
     optionBTitle = data.get('poll-option-b');
-
     //display current polls
+    displayCurrentPoll();
+    //clear DOM
+    form.reset();
+});
+
+// set event listener for "close poll" button to display past polls
+closePollButton.addEventListener('click', () => {
+    // save a copy of the state (past poll) in an object to be pushed into an array ==> create a makePoll()
+    const poll = makePoll();
+    // push past poll object into an array so we have a history of past polls
+    pastPollsArray.push(poll);
+    //reset our state for a new question now that we have saved a copy
+    question = '';
+    optionATitle = '';
+    optionBTitle = '';
+    optionAVotes = 0;
+    optionBVotes = 0;
+
+    // empty the current poll div (reset the DOM with this new reset state)
+    displayCurrentPoll();
+
+    // display a list of all past polls
+    displayAllPolls ();
+    
+});
+
+
+export function displayCurrentPoll() {
     questionEl.textContent = question;
     optionATitleEl.textContent = optionATitle;
     optionBTitleEl.textContent = optionBTitle;
     optionAVotesEl.textContent = optionAVotes;
     optionBVotesEl.textContent = optionBVotes;
-});
+}
 
+export function makePoll() {
+    return {
+        question: question,
+        optionATitle: optionATitle,
+        optionBTitle: optionBTitle,
+        optionAVotes: optionAVotes,
+        optionBVotes: optionBVotes,
+    };
+}
 
-// set event listener for "close poll" button to display past polls
-closePollButton.addEventListener('click', () => {
+export function displayAllPolls() {
+  // for each item
+    //clear out the text content of the list in the DOM
+    pastPollsEl.textContent = '';
 
-
-
-
-
-
-});
-
-
-
-
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+    for (let pastPoll of pastPollsArray) {
+      // create new elements without mutating outside the loop
+        const pastPolls = renderPoll(pastPoll);
+        // append container to the DOM Element
+        pastPollsEl.append(pastPolls);
+    }
+}
--- render-utils.js
+++ render-utils.js
@@ -1,11 +1,27 @@
-export function renderPastPoll() {
+export function renderPoll(pastPollData) {
+     // create new elements without mutating outside the loop
+    const container = document.createElement('div');
+    const pQuestionEl = document.createElement('p');
+    const optionADiv = renderOption(pastPollData.optionATitle, pastPollData.optionAVotes);
+    const optionBDiv = renderOption(pastPollData.optionBTitle, pastPollData.optionBVotes);
 
+    container.append(pQuestionEl, optionADiv, optionBDiv);
+    container.classList.add('past-poll-data');
 
+    pQuestionEl.textContent = `Question: ${pastPollData.question}`;
 
+    return container;
 
+}
 
+export function renderOption(option, votes) {
+    const pastPollDiv = document.createElement('div');
+    const optionDiv = document.createElement('p');
+    const voteDiv = document.createElement('p');
 
-
-
+    optionDiv.textContent = option;
+    voteDiv.textContent = votes;
     
+    pastPollDiv.append(optionDiv, voteDiv);
+    return pastPollDiv;
 }
